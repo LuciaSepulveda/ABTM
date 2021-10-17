4d8ac331e5b33697ae8bd42ec4fad28406293bbd
LuciaSepulveda <luciabsep18@gmail.com>
2021-10-17 12:03:28 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type {NextPage} from "next"
+import Image from "next/image"
+import {Box, Flex} from "@chakra-ui/react"
+import React from "react"
+
+const Home: NextPage = () => {
+  return (
+    <Flex minHeight="100vh">
+      <header>
+        <Box>hola</Box>
+      </header>
+    </Flex>
+  )
+}
+
+export default Home
