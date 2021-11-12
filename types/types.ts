b6d8edc14714af11a566862e066db5d4e544952f
LuciaSepulveda <luciabsep18@gmail.com>
2021-11-12 12:32:35 -0300

    update types and data

--- types/types.ts
+++ types/types.ts
@@ -2,14 +2,27 @@ export interface New {
   title: string
   date: string
   description: string
-  photo: string
+  short_description: string
+  photo: {
+    url: string
+  }
+  id: number
 }
 
 export interface Calendar {
   title: string
   date: string
   description: string
-  photo: string
+  photo: {
+    url: string
+  }
+  id: number
+}
+
+export interface Autoridad {
+  position: string
+  name: string
+  id: number
 }
 
 export enum Page {
@@ -20,4 +33,72 @@ export enum Page {
   TenisDeMesa = "tenisdemesa",
   Reglamentos = "reglamentos",
   Inscripcion = "inscripcion",
+}
+
+export interface Puntaje {
+  posicion: number
+  quinta: number
+  cuarta: number
+  tercera: number
+  segunda: number
+  primera: number
+  SD: number
+}
+
+export interface PuntajeRating {
+  diferencia: string
+  jugadorPuntajeMayor: number
+  jugadorPuntajeMenor: number
+}
+
+export interface CategoriaRating {
+  categoria: string
+  puntajeIngreso: number
+  puntajeMinimo: number
+  puntajeMaximo: number
+}
+
+export interface Categorias {
+  categorias: CategoriaRating[]
+}
+
+export interface Pdf {
+  name: string
+  file: {
+    url: string
+    id: number
+  }
+  id: number
+}
+
+export interface Ranking {
+  position: number
+  name: string
+  points: number
+  id: number
+}
+
+export interface Rules {
+  name: string
+  rules: string[]
+}
+
+export interface Inscripto {
+  dni: string
+  nombre: string
+  apellido: string
+  email: string
+  telefono: string
+  provincia: string
+  ciudad: string
+  categoria: string
+  comentario: string
+  id: number
+}
+
+export interface Photo {
+  id: number
+  photo: {
+    url: string
+  }
 }
