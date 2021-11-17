dccab3e06251a93f9e1afbbbf3e17be7b6d19f18
LuciaSepulveda <luciabsep18@gmail.com>
2021-11-17 16:10:34 -0300

    update news

--- components/index/News.tsx
+++ components/index/News.tsx
@@ -2,37 +2,25 @@ import {Box, Center, Container, Text, VStack} from "@chakra-ui/react"
 import React from "react"
 import Image from "next/image"
 import {useMediaQuery} from "react-responsive"
-import {Carousel} from "@trendyol-js/react-carousel"
 import {MdKeyboardArrowLeft, MdKeyboardArrowRight} from "react-icons/md"
-import {motion} from "framer-motion"
 import Link from "next/link"
-
-import {New} from "../../types/types"
-
+import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
+
+import {New} from "../../types/types"
 
 interface Props {
   news: New[]
 }
 
-const MotionVStack = motion(VStack)
-
-const News2: React.FC<Props> = ({news}) => {
+const News: React.FC<Props> = ({news}) => {
   const isPortrait = useMediaQuery({query: "(orientation: portrait)"})
 
   return (
-    <Center minH="100vh" p={isPortrait ? 0 : 2} paddingTop={10} position="relative" w="100%">
-      <Container maxW="8xl">
-        <MotionVStack
-          initial={{opacity: 0}}
-          m="auto"
-          spacing={24}
-          transition={{duration: 0.5}}
-          viewport={{once: true}}
-          w={["100%", null, "100%"]}
-          whileInView={{opacity: 1}}
-        >
+    <VStack minH="100vh" p={isPortrait ? 0 : 2} paddingTop={10} w="100%">
+      <Container h="100%" maxW="8xl">
+        <VStack m="auto" mb={20} spacing={24} w="100%">
           <Text
             color="#3C6ECD"
             fontSize="6xl"
@@ -43,32 +31,14 @@ const News2: React.FC<Props> = ({news}) => {
           >
             Noticias
           </Text>
-          <Carousel
-            leftArrow={
-              <Center
-                style={{
-                  width: isPortrait ? "30px" : "50px",
-                  height: "100%",
-                }}
-              >
-                <Box
-                  _hover={{backgroundColor: "#3C6ECD", color: "white"}}
-                  as="button"
-                  border="1px solid #3C6ECD"
-                  borderRadius="12px"
-                  color="#3C6ECD"
-                  fontSize="16px"
-                  fontWeight="bold"
-                  h="30px"
-                  paddingX={2}
-                  paddingY={0}
-                  transition="all ease-in 0.2s"
-                >
-                  <MdKeyboardArrowLeft />
-                </Box>
-              </Center>
-            }
-            rightArrow={
+        </VStack>
+        <Slider
+          dots
+          infinite
+          nextArrow={
+            isPortrait ? (
+              <Text>{`<`}</Text>
+            ) : (
               <Center
                 style={{
                   width: isPortrait ? "30px" : "50px",
@@ -91,82 +61,114 @@ const News2: React.FC<Props> = ({news}) => {
                   <MdKeyboardArrowRight />
                 </Box>
               </Center>
-            }
-            show={isPortrait ? 1 : 3}
-            slide={1}
-            swiping={true}
-          >
-            {news.map((n) => (
-              <Link key={n.id} passHref href={`/noticias/${n.id}`}>
-                <a>
-                  <VStack
-                    border="2px solid #EBEAED"
-                    borderRadius="10px"
-                    h={500}
-                    justify="center"
-                    w={[300, null, 400]}
+            )
+          }
+          prevArrow={
+            isPortrait ? (
+              <Text>{`<`}</Text>
+            ) : (
+              <Center
+                style={{
+                  width: isPortrait ? "30px" : "50px",
+                  height: "100%",
+                }}
+              >
+                <Box
+                  _hover={{backgroundColor: "#3C6ECD", color: "white"}}
+                  as="button"
+                  border="1px solid #3C6ECD"
+                  borderRadius="12px"
+                  color="#3C6ECD"
+                  fontSize="16px"
+                  fontWeight="bold"
+                  h="30px"
+                  paddingX={2}
+                  paddingY={0}
+                  transition="all ease-in 0.2s"
+                >
+                  <MdKeyboardArrowLeft />
+                </Box>
+              </Center>
+            )
+          }
+          slidesToScroll={1}
+          slidesToShow={isPortrait ? 1 : 3}
+          speed={500}
+        >
+          {news.map((n) => (
+            <Box key={n.id} p={4}>
+              <VStack
+                border="2px solid #EBEAED"
+                borderRadius="10px"
+                h={500}
+                justify="center"
+                spacing={0}
+              >
+                <Box h="100%" overflow="hidden" position="relative" w="100%">
+                  <Box
+                    _hover={{transform: "scale(1.4)"}}
+                    borderTopRadius="10px"
+                    h="100%"
+                    overflow="hidden"
+                    position="relative"
+                    transition="all ease-in 0.3s"
+                    w="100%"
                   >
-                    <Box h="100%" overflow="hidden" position="relative" w="100%">
-                      <Box
-                        _hover={{transform: "scale(1.4)"}}
-                        borderTopRadius="10px"
-                        h="100%"
-                        overflow="hidden"
-                        position="relative"
-                        transition="all ease-in 0.3s"
-                        w="100%"
-                      >
-                        <Image
-                          alt={"Foto de " + n.title}
-                          blurDataURL={`${n.photo.url}`}
-                          layout="fill"
-                          objectFit="cover"
-                          placeholder="blur"
-                          src={n.photo.url}
-                        />
-                      </Box>
-                    </Box>
-                    <Box h={14} overflowY="hidden" p={1} w="100%">
-                      <Text color="rgba(21, 20, 57, 1)" fontSize="14px" ml={4} textAlign="left">
-                        {n.date}
-                      </Text>
-                    </Box>
-                    <Box
-                      borderTop="1px solid rgba(21, 20, 57, 0.2)"
-                      h={14}
-                      overflowY="hidden"
-                      p={0.5}
-                      w="100%"
-                    >
+                    <Image
+                      alt={"Foto de " + n.title}
+                      blurDataURL={`${n.photo.formats.small.url}`}
+                      layout="fill"
+                      objectFit="cover"
+                      placeholder="blur"
+                      src={n.photo.url}
+                    />
+                  </Box>
+                </Box>
+                <Box h={14} overflowY="hidden" p={1} w="100%">
+                  <Text color="rgba(21, 20, 57, 1)" fontSize="14px" ml={4} textAlign="left">
+                    {n.date}
+                  </Text>
+                </Box>
+                <Box
+                  borderTop="1px solid rgba(21, 20, 57, 0.2)"
+                  h={[20, null, 14]}
+                  overflowY="hidden"
+                  p={0.5}
+                  w="100%"
+                >
+                  <Link passHref href={`/noticias/${n.id}`}>
+                    <a>
                       <Text
+                        _hover={{color: "#3C6ECD"}}
                         color="rgba(30, 14, 98, 1)"
                         fontWeight="bold"
                         m="auto"
                         textAlign="center"
+                        textDecoration="underline rgba(30, 14, 98, 1)"
                       >
                         {n.title}
                       </Text>
-                    </Box>
-                    <Box
-                      borderTop="1px solid rgba(21, 20, 57, 0.2)"
-                      h={48}
-                      overflowY="hidden"
-                      p={0.5}
-                      w="100%"
-                    >
-                      <Text color="rgba(21, 20, 57, 0.6)" textAlign="center">
-                        {n.short_description + "Hola no se que mas [poner para probnar"}
-                      </Text>
-                    </Box>
-                  </VStack>
-                </a>
-              </Link>
-            ))}
-          </Carousel>
-        </MotionVStack>
+                    </a>
+                  </Link>
+                </Box>
+                <Box
+                  borderTop="1px solid rgba(21, 20, 57, 0.2)"
+                  h={48}
+                  overflowY="hidden"
+                  p={0.5}
+                  w="100%"
+                >
+                  <Text color="rgba(21, 20, 57, 0.6)" textAlign="center">
+                    {n.short_description}
+                  </Text>
+                </Box>
+              </VStack>
+            </Box>
+          ))}
+        </Slider>
       </Container>
-    </Center>
+    </VStack>
   )
 }
 
-export default News2
+export default News
