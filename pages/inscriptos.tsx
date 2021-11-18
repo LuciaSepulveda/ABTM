b95a74d1b6d54d67d3b50e19856d58cfb798419b
LuciaSepulveda <luciabsep18@gmail.com>
2021-11-18 17:12:35 -0300

    try new data fetching inscriptos

--- pages/inscriptos.tsx
+++ pages/inscriptos.tsx
@@ -1,6 +1,5 @@
-import {Box, Container, Table, Text, Tbody, Td, Th, Thead, VStack} from "@chakra-ui/react"
+import {Box, Container, Table, Text, Tbody, Td, Th, Thead, VStack, Spinner} from "@chakra-ui/react"
 import axios from "axios"
-import {GetStaticProps} from "next"
 import React from "react"
 
 import Footer from "../components/Footer"
@@ -8,17 +7,24 @@ import Menu from "../components/Menu"
 import {useChangePage, usePage} from "../context/hooks"
 import {Inscripto, Page} from "../types/types"
 
-interface Props {
-  inscriptos: Inscripto[]
-}
-
-const Inscriptos: React.FC<Props> = ({inscriptos}) => {
+const Inscriptos: React.FC = () => {
+  const [status, setStatus] = React.useState<boolean>(false)
+  const [inscriptos, setInscriptos] = React.useState<Inscripto[]>([])
   const changePage = useChangePage()
   const page = usePage()
 
   React.useEffect(() => {
     if (page !== Page.Inscripcion) changePage(Page.Inscripcion)
   }, [page, changePage])
+
+  React.useEffect(() => {
+    if (!status) {
+      axios.get("https://strapi-abtm.herokuapp.com/inscriptos").then((res) => {
+        setInscriptos(res.data)
+        setStatus(true)
+      })
+    }
+  }, [status])
 
   return (
     <VStack
@@ -35,51 +41,36 @@ const Inscriptos: React.FC<Props> = ({inscriptos}) => {
           <Text as="h2" fontSize="6xl" fontWeight="bold" textAlign="center">
             Inscriptos
           </Text>
-          <Box overflowX={["scroll", null, "hidden"]} w={["100%", null, "80%"]}>
-            <Table colorScheme="facebook" p={[0, null, 4]} variant="simple">
-              <Thead bg="#3c6fcd88">
-                <Th color="#242424" textAlign="center">
-                  Nombre
-                </Th>
-                <Th color="#242424" textAlign="center">
-                  Apellido
-                </Th>
-                <Th color="#242424" textAlign="center">
-                  Categoria
-                </Th>
-              </Thead>
-              {inscriptos.map((inscripto) => (
-                <Tbody key={inscripto.id}>
-                  <Td textAlign="center">{inscripto.nombre}</Td>
-                  <Td textAlign="center">{inscripto.apellido}</Td>
-                  <Td textAlign="center">{inscripto.categoria}</Td>
-                </Tbody>
-              ))}
-            </Table>
-          </Box>
+          {!status && <Spinner />}
+          {status && (
+            <Box overflowX={["scroll", null, "hidden"]} w={["100%", null, "80%"]}>
+              <Table colorScheme="facebook" p={[0, null, 4]} variant="simple">
+                <Thead bg="#3c6fcd88">
+                  <Th color="#242424" textAlign="center">
+                    Nombre
+                  </Th>
+                  <Th color="#242424" textAlign="center">
+                    Apellido
+                  </Th>
+                  <Th color="#242424" textAlign="center">
+                    Categoria
+                  </Th>
+                </Thead>
+                {inscriptos.map((inscripto) => (
+                  <Tbody key={inscripto.id}>
+                    <Td textAlign="center">{inscripto.nombre}</Td>
+                    <Td textAlign="center">{inscripto.apellido}</Td>
+                    <Td textAlign="center">{inscripto.categoria}</Td>
+                  </Tbody>
+                ))}
+              </Table>
+            </Box>
+          )}
         </VStack>
       </Container>
       <Footer />
     </VStack>
   )
-}
-
-export const getStaticProps: GetStaticProps<Props, never> = async () => {
-  const res = await axios.get<Inscripto[]>("https://strapi-abtm.herokuapp.com/inscriptos")
-  const inscriptos = res.data
-
-  if (!inscriptos) {
-    return {
-      notFound: true,
-    }
-  }
-
-  return {
-    props: {
-      inscriptos: inscriptos,
-    },
-    revalidate: 1,
-  }
 }
 
 export default Inscriptos
